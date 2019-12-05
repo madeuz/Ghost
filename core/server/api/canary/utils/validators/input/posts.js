e2311327709e4788f769de1241dcff6bfaae9aa6
Mateusz Szczygielski <mateusz.szczygielski@gmail.com>
2019-12-05 10:37:40 +0100

    Validate post on publish

--- core/server/api/canary/utils/validators/input/posts.js
+++ core/server/api/canary/utils/validators/input/posts.js
@@ -1,15 +1,44 @@
 const jsonSchema = require('../utils/json-schema');
+const common = require('../../../../../lib/common');
+
+const validateForPublish = (post) => {
+    if (post.tags.length === 0) {
+        return Promise.reject(new common.errors.ValidationError({message: 'Please provide at least one tag.'}));
+    }
+    if (!post.feature_image) {
+        return Promise.reject(new common.errors.ValidationError({message: 'Please provide featured image.'}));
+    }
+    return true;
+}
 
 module.exports = {
     add(apiConfig, frame) {
         const schema = require(`./schemas/posts-add`);
         const definitions = require('./schemas/posts');
+        const data = frame.data.posts[0];
+        
+        if (data.status !== 'draft') {
+            const validForPublish = validateForPublish(data);
+            if (validForPublish !== true) {
+                return validForPublish;
+            }
+        }
+
         return jsonSchema.validate(schema, definitions, frame.data);
     },
 
     edit(apiConfig, frame) {
         const schema = require(`./schemas/posts-edit`);
         const definitions = require('./schemas/posts');
+        const data = frame.data.posts[0];
+
+        if (data.status !== 'draft') {
+            const validForPublish = validateForPublish(data);
+            if (validForPublish !== true) {
+                return validForPublish;
+            }
+        }
+
         return jsonSchema.validate(schema, definitions, frame.data);
     }
 };
